d3951bf84e7aa475ad1f40ab2e57c60cd60c6c40
Sharmin <sharmee236@yahoo.com>
2017-11-01 09:54:36 -0400

    first check in of creatingGitFile

---
+++ CreatingGitFile/src/firstFolder/CodeForGit.java
@@ -0,0 +1,25 @@
+package firstFolder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CodeForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d = new Date();
+		System.out.println(d.toString());
+		
+		//format mm/dd/yyyy HH:MM:SS
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/d/yyyy HH:mm:SS");
+		System.out.println(sdf.format(d));
+		
+		//www.tutorialspoint.com/java/java_date_time.htm
+		
+		
+
+
+	}
+
+}
